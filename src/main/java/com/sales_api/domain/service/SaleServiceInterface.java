a046dcb4b8d6dac4bcb5acd9a58e4b1b6e54bed2
JoaoBenignodev <joao.benigno@aluno.faculdadeimpacta.com.br>
2024-09-29 12:34:21 -0300

    SaleController setup

--- src/main/java/com/sales_api/domain/service/SaleServiceInterface.java
+++ src/main/java/com/sales_api/domain/service/SaleServiceInterface.java
@@ -1,4 +1,11 @@
 package com.sales_api.domain.service;
 
-public class SaleServiceInterface {
+import com.sales_api.domain.dtos.request.SaleRequestDto;
+import com.sales_api.domain.dtos.response.SaleResponseDto;
+
+public interface SaleServiceInterface {
+    SaleResponseDto save(SaleRequestDto saleRequestDto);
+    SaleResponseDto getSale(Long id);
+    SaleResponseDto updateSale(Long id, SaleRequestDto saleRequestDto);
+    SaleResponseDto deleteSale(Long id);
 }
